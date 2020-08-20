c4f3ed960eddb83c818c892cb51ab275eb75284c
Bryan Chiu <1930893235@qq.com>
2020-08-20 22:54:46 +0800

    2020/8/20 加代码注释
    Former-commit-id: 5b4b3aaf65dad5295d3574e2ee1f87d3b462fac2

--- mirai-demo/dllmain.cpp
+++ mirai-demo/dllmain.cpp
@@ -20,11 +20,24 @@ BOOL APIENTRY DllMain(HMODULE hModule,
 /*
 *正文开始
 */
+#define no "CONTINUE" //用于返回发送信息时，返回该值不会发送信息
 #include <iostream>
+    /*
+    * 名称:Java_com_example_plugin_CPP_1lib_FriendRequest
+    * 作用:好友申请
+    * 参数:env 必备参数，job 必备参数，qqid qq号，nick 申请者昵称，message 验证消息
+    * 返回值:bool 是为添加 否为不添加
+    */
     JNIEXPORT jboolean JNICALL Java_com_example_plugin_CPP_1lib_FriendRequest
-    (JNIEnv*, jobject, jlong, jstring, jstring) {
+    (JNIEnv* env, jobject job, jlong qqid, jstring nick, jstring message) {
         return true;
     }
+    /*
+    * 名称:str2jstring
+    * 作用:string类型转jstring类型
+    * 参数:JNIEnv,const char*(string)
+    * 返回值:jstring
+    */
     jstring str2jstring(JNIEnv* env, const char* pat)
     {
         //定义java String类 strClass
@@ -40,15 +53,33 @@ BOOL APIENTRY DllMain(HMODULE hModule,
         //将byte数组转换为java String,并输出
         return (jstring)(env)->NewObject(strClass, ctorID, bytes, encoding);
     }
-    JNIEXPORT jstring JNICALL Java_com_example_plugin_CPP_1lib_Verify(JNIEnv* e, jobject) {
-        return str2jstring(e,"2333");//验证机制
+    /*
+    * 名称:Java_com_example_plugin_CPP_1lib_Verify
+    * 作用:判断是否连接上本插件，勿改
+    * 参数:env 必备，job 必备
+    * 返回值:jstring (用str2jstring把string类型转成jsrting) 发送返回的字符串
+    */
+    JNIEXPORT jstring JNICALL Java_com_example_plugin_CPP_1lib_Verify(JNIEnv* env, jobject job) {
+        return str2jstring(env,"2333");//验证机制
     }
+    /*
+    * 名称:Java_com_example_plugin_CPP_1lib_PrivateMessage
+    * 作用:处理私聊消息
+    * 参数:env 必备job 必备，qqid qq号，message 发送的消息
+    * 返回值:jstring(用str2jstring把string类型转成jsrting) 发送返回的字符串
+    */
     JNIEXPORT jstring JNICALL Java_com_example_plugin_CPP_1lib_PrivateMessage
-    (JNIEnv* e, jobject, jlong, jstring) {
-        return str2jstring(e,"我们还在测试阶段");
+    (JNIEnv* env, jobject job, jlong qqid, jstring message) {
+        return str2jstring(env,"我们还在测试阶段");
 
     }
+    /*
+    * 名称:Java_com_example_plugin_CPP_1lib_GroupMessage
+    * 作用:处理群消息
+    * 参数:env:JNIEnv 必备， job:jobject 必备，groupid:jlong 群号，qqid:jlong qq号，message:jstring 信息
+    * 返回值:jsrting (用str2jstring把string类型转成jsrting) 发送返回的字符串
+    */
     JNIEXPORT jstring JNICALL Java_com_example_plugin_CPP_1lib_GroupMessage
-    (JNIEnv* e, jobject, jlong, jlong, jstring) {
-        return str2jstring(e,"CONTINUE");
+    (JNIEnv* env, jobject job, jlong groupid, jlong qqid, jstring message) {
+        return str2jstring(env,no);
     }
