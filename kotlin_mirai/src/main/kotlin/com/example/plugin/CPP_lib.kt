ac25a953d3fdb3f8322459c76ef6d86367241a7e
Bryan Chiu <1930893235@qq.com>
2020-09-18 09:00:55 +0800

    2020/9/18 prerelease
    Former-commit-id: 0e9e65b1f9b7aed14423945af54ac86739a9073b
    Former-commit-id: 950e9e7c0ef2ec270726da0b1685edaebe08c9ab

--- kotlin_mirai/src/main/kotlin/com/example/plugin/CPP_lib.kt
+++ kotlin_mirai/src/main/kotlin/com/example/plugin/CPP_lib.kt
@@ -1,16 +1,32 @@
 package com.example.plugin
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
 class CPP_lib {
     var ver:String=""
     init {
         ver=Verify()//如果ver不等于2333 载入错误
     }
-    companion object {
+
+    companion object{
         init {
             //这里填自己的路径
             System.load("D:\\Mirai\\libraries\\mirai-demo.dll")
         }
+        @JvmStatic
+        fun SendPrivate(message: String, id: Long) {
+            GlobalScope.launch(Dispatchers.Main) {
+                // 反向调用发送消息 测试中
+                Send(message, id)
+            }
+        }
     }
+
+    
+
     external fun Verify(): String
     external fun PrivateMessage(id:Long,nick:String,avatarUrl:String, message:String): String
     external fun GroupMessage(groupId:Long,GroupName:String,ownerId:Long,MemberName:String,MemberGroupName:String,Memberid: Long,message:String): String
@@ -19,3 +35,4 @@ class CPP_lib {
     external fun GroupMemberLeave(Memberid:Long,Q:Boolean):String
     external fun GroupMemberJoin(Memberid:Long,Driving:Boolean):String
 }
+
