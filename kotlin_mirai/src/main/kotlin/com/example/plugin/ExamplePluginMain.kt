7891ebe700f1ede845043216afd835bb7a93d94f
Bryan Chiu <1930893235@qq.com>
2020-10-29 22:19:45 +0800

    2020/10/29 PRErelease
    Former-commit-id: 11508d61f9dac20316eecc23ae23e2e413bfb9ce
    Former-commit-id: 0282189b1e72371ef20d5dfb05e952698aabc6a7

--- kotlin_mirai/src/main/kotlin/com/example/plugin/ExamplePluginMain.kt
+++ kotlin_mirai/src/main/kotlin/com/example/plugin/ExamplePluginMain.kt
@@ -5,82 +5,93 @@
  */
 package com.example.plugin
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.plugins.PluginBase
 import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.event.subscribeAlways
-import net.mamoe.mirai.message.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.Dispatchers
+import net.mamoe.mirai.message.FriendMessageEvent
+import net.mamoe.mirai.message.GroupMessageEvent
 
 object ExamplePluginMain : PluginBase() {
-    lateinit var AIbot:Bot
-    fun Send(message:String, id:Long){
+    private lateinit var AIbot: Bot
+    suspend fun Send(message: String, id: Long) {
         //反向调用
         logger.info("Send message for $id is $message")
-        val a=AIbot.getFriend(id)
-        GlobalScope.launch(Dispatchers.Unconfined) {
-            a.sendMessage(message)
-        }
+        val a = AIbot.getFriend(id)
+        a.sendMessage(message)
     }
-    fun BasicSendLog(log:String){
+
+    fun BasicSendLog(log: String) {
         logger.info(log);
     }
-    fun SendG(message:String,id:Long){
-        GlobalScope.launch(Dispatchers.Unconfined) {
-            logger.info("Send message for Group($id) is $message")
-            AIbot.getGroup(id).sendMessage(message)
+
+    suspend fun SendG(message: String, id: Long) {
+        logger.info("Send message for Group($id) is $message")
+        AIbot.getGroup(id).sendMessage(message)
+    }
+
+    fun GetN(qqid: Long): String {
+        return AIbot.getFriend(qqid).nick
+    }
+
+    override fun onEnable() {
+        val cpp = CPP_lib()
+        super.onEnable()
+        //插件已加载
+        logger.info("Plugin loaded!")
+        logger.info("github存储库:https://github.com/Nambers/MiraiCP")
+        logger.info(cpp.ver)//输出2333 正常
+        subscribeAlways<BotOnlineEvent> {
+            AIbot = this.bot
         }
-    }
-    fun GetN(qqid:Long):String {
-        return AIbot.getFriend(qqid).nick;
-    }
-        override fun onEnable() {
-            val cpp=CPP_lib()
-            super.onEnable()
-            //插件已加载
-            logger.info("Plugin loaded!")
-            logger.info("github存储库:https://github.com/Nambers/MiraiCP")
-            logger.info(cpp.ver)//输出2333 正常
-            subscribeAlways<BotOnlineEvent> {
-                AIbot=this.bot
+        subscribeAlways<GroupMessageEvent> {
+            //群信息
+            GlobalScope.launch(Dispatchers.Default) {
+                cpp.GroupMessage(
+                    subject.id,
+                    subject.name,
+                    subject.owner.id,
+                    sender.nick,
+                    sender.nameCard,
+                    sender.id,
+                    message.toString()
+                )
             }
-            subscribeAlways<GroupMessageEvent> {
-                //群信息
-                cpp.GroupMessage(subject.id,subject.name,subject.owner.id, sender.nick,sender.nameCard,sender.id, message.toString())
+        }
+        subscribeAlways<FriendMessageEvent> {
+            //个人信息
+            logger.info("starting")
+            cpp.PrivateMessage(subject.id,message.toString())
+            logger.info("ending")
+        }
+        subscribeAlways<MemberJoinEvent.Active> {
+            cpp.GroupMemberJoin(member.id, true, group.id)
+        }
+        subscribeAlways<MemberJoinEvent.Invite> {
+            cpp.GroupMemberJoin(member.id, false, group.id)
+        }
+        subscribeAlways<MemberLeaveEvent.Kick> {
+            cpp.GroupMemberLeave(member.id, true, group.id)
+        }
+        subscribeAlways<MemberLeaveEvent.Quit> {
+            cpp.GroupMemberLeave(member.id, false, group.id)
+        }
+        subscribeAlways<GroupNameChangeEvent> {
+            val a: Long = if (operator == null) 0
+            else operator!!.id
+            cpp.GroupNameChange(origin, new, group.id, a)
+        }
 
-            }
-            subscribeAlways<FriendMessageEvent> {
-                //个人信息
-                cpp.PrivateMessage(subject.id,subject.nick,subject.avatarUrl, message.toString())
-            }
-            subscribeAlways<MemberJoinEvent.Active> {
-                cpp.GroupMemberJoin(member.id, true,group.id)
-            }
-            subscribeAlways<MemberJoinEvent.Invite> {
-                cpp.GroupMemberJoin(member.id, false,group.id)
-            }
-            subscribeAlways<MemberLeaveEvent.Kick> {
-                cpp.GroupMemberLeave(member.id, true,group.id)
-            }
-            subscribeAlways<MemberLeaveEvent.Quit> {
-                cpp.GroupMemberLeave(member.id, false,group.id)
-            }
-            subscribeAlways<GroupNameChangeEvent> {
-                val a:Long
-                if(operator==null) a=0
-                else a= operator!!.id
-                cpp.GroupNameChange(origin, new, group.id, a)
-            }
-
-            subscribeAlways<NewFriendRequestEvent> {
-                //自动同意加好友
-                if(cpp.FriendRequest(fromId, fromNick, message)) accept()
-                else reject()
-            }
-
+        subscribeAlways<NewFriendRequestEvent> {
+            //自动同意加好友
+            if (cpp.FriendRequest(fromId, fromNick, message)) accept()
+            else reject()
         }
 
     }
+
+}
 
