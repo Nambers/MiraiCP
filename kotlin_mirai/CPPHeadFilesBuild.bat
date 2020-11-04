baff08b1b004aeea0c7f6c9c415dc4fa9ba1a8b0
Bryan Chiu <1930893235@qq.com>
2020-11-04 08:17:19 +0800

    2020/11/4 FIX Bugs
    Former-commit-id: 1b06644209ff1656ac199cce03255e05000c1343
    Former-commit-id: 46e43c6abff5325f2c19f9b46f0ba77caf3fba98

--- kotlin_mirai/CPPHeadFilesBuild.bat
+++
@@ -1,6 +0,0 @@
-echo "please run the classes first"
-echo "please change new line address to your"
-cd D:\VS_Project\MiraiCP\kotlin_mirai\build\classes\kotlin\main\com\example\plugin
-javah com.example.plugin.CPP_lib
-echo ".h files are in the main"
-pause
