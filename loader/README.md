d6584ee726ad84325e47aa5a544cc35c7699b9f7
Eritque arcus <1930893235@qq.com>
2021-02-15 16:22:10 +0800

    增加SDK整合包形式(/loader)

---
+++ loader/README.md
@@ -0,0 +1,5 @@
+# mirai-hello-world
+
+Mirai Hello World example.
+
+Run `src/main/kotlin/KotlinMain.kt` or `src/kotlin/java/org/example/mirai/JavaMain.java`
---
+++ loader/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+
+plugins {
+    java
+    application
+    val kotlinVersion = "1.4.30"
+    kotlin("jvm") version kotlinVersion
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+
+}
+application {
+    mainClass.set("org.example.mirai.plugin.KotlinMainKt")
+    mainClassName = "org.example.mirai.plugin.KotlinMainKt"
+}
+group = "org.example"
+version = "0.1.0"
+
+repositories {
+    //国内镜像源
+    //华为云
+    maven { url =uri("https://mirrors.huaweicloud.com/repository/maven") }
+    //阿里云
+    maven { url =uri("https://maven.aliyun.com/nexus/content/repositories/jcenter")}
+    //mirai-console的仓库
+    maven {url = uri("https://dl.bintray.com/him188moe/mirai")}
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
+
+tasks.withType(KotlinJvmCompile::class.java) {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+dependencies {
+    implementation("org.fusesource.jansi:jansi:1.18")
+    implementation("com.google.code.gson:gson:2.8.6")
+    api("net.mamoe:mirai-core:2.4.0")
+}
