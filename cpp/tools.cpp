56e79eaa1a22b579574d7b163a75dffc14459884
Eritque arcus <1930893235@qq.com>
2021-05-05 12:25:01 +0800

    合并日志,发送,取昵称或名称和上传图片接口 #38

--- cpp/tools.cpp
+++ cpp/tools.cpp
@@ -10,30 +10,21 @@ void Config::Init(JNIEnv* env) {
 	if (this->CPP_lib == NULL) {
 		throw InitException("初始化错误", 1);
 	}
-	this->Query = env->GetStaticMethodID(CPP_lib, "QueryImgUrl", "(Ljava/lang/String;)Ljava/lang/String;");
-	this->SendMsg2F = env->GetStaticMethodID(CPP_lib, "SendPrivateMSG", "(Ljava/lang/String;J)Ljava/lang/String;");
-	this->SendMsg2FM = env->GetStaticMethodID(CPP_lib, "SendPrivateMSGM", "(Ljava/lang/String;J)Ljava/lang/String;");
-	this->NickorNameF = env->GetStaticMethodID(CPP_lib, "GetNick", "(J)Ljava/lang/String;");
-	this->SendMsg2M = env->GetStaticMethodID(CPP_lib, "SendPrivateM2M", "(Ljava/lang/String;JJ)Ljava/lang/String;");
-	this->SendMsg2MM = env->GetStaticMethodID(CPP_lib, "SendPrivateM2MM", "(Ljava/lang/String;JJ)Ljava/lang/String;");
-	this->NickorNameM = env->GetStaticMethodID(CPP_lib, "GetNameCard", "(JJ)Ljava/lang/String;");
-	this->SendMsg2G = env->GetStaticMethodID(CPP_lib, "SendGroup", "(Ljava/lang/String;J)Ljava/lang/String;");
-	this->SendMsg2GM = env->GetStaticMethodID(CPP_lib, "SendGroupM", "(Ljava/lang/String;J)Ljava/lang/String;");
-	this->Schedule = env->GetStaticMethodID(CPP_lib, "schedule", "(JLjava/lang/String;)V");
-	this->Mute = env->GetStaticMethodID(CPP_lib, "muteM", "(JJI)Ljava/lang/String;");
-	this->QueryP = env->GetStaticMethodID(CPP_lib, "queryM", "(JJ)Ljava/lang/String;");
-	this->KickM = env->GetStaticMethodID(CPP_lib, "kickM", "(JJLjava/lang/String;)Ljava/lang/String;");
-	this->recallMsgM = env->GetStaticMethodID(CPP_lib, "recall", "(Ljava/lang/String;)Ljava/lang/String;");
-	this->QueryML = env->GetStaticMethodID(CPP_lib, "queryML", "(J)Ljava/lang/String;");
-	this->QueryN = env->GetStaticMethodID(CPP_lib, "queryNG", "(J)Ljava/lang/String;");
-	this->uploadImgF= env->GetStaticMethodID(config->CPP_lib, "uploadImgF", "(JLjava/lang/String;)Ljava/lang/String;");
-	this->uploadImgG = env->GetStaticMethodID(config->CPP_lib, "uploadImgG", "(JLjava/lang/String;)Ljava/lang/String;");
-	this->uploadImgM = env->GetStaticMethodID(config->CPP_lib, "uploadImgM", "(JJLjava/lang/String;)Ljava/lang/String;");
-	this->muteAll = env->GetStaticMethodID(config->CPP_lib, "muteGroup", "(JZ)Ljava/lang/String;");
-	this->getowner = env->GetStaticMethodID(config->CPP_lib, "queryOwner", "(J)Ljava/lang/String;");
-	this->buildforward = env->GetStaticMethodID(config->CPP_lib, "buildforward", "(Ljava/lang/String;)Ljava/lang/String;");
-	this->NFR = env->GetStaticMethodID(config->CPP_lib, "nfroperation", "(Ljava/lang/String;Z)Ljava/lang/String;");
-	this->GI = env->GetStaticMethodID(config->CPP_lib, "gioperation", "(Ljava/lang/String;Z)Ljava/lang/String;");
+	this->Send = env->GetStaticMethodID(CPP_lib, "KSend", "(Ljava/lang/String;Z)Ljava/lang/String;");
+	this->Query = env->GetStaticMethodID(CPP_lib, "KQueryImgUrl", "(Ljava/lang/String;)Ljava/lang/String;");
+	this->NickorName = env->GetStaticMethodID(CPP_lib, "KGetNickOrNameCard", "(Ljava/lang/String;)Ljava/lang/String;");
+	this->Schedule = env->GetStaticMethodID(CPP_lib, "KSchedule", "(JLjava/lang/String;)V");
+	this->Mute = env->GetStaticMethodID(CPP_lib, "KMuteM", "(JJI)Ljava/lang/String;");
+	this->QueryP = env->GetStaticMethodID(CPP_lib, "KQueryM", "(JJ)Ljava/lang/String;");
+	this->KickM = env->GetStaticMethodID(CPP_lib, "KKickM", "(JJLjava/lang/String;)Ljava/lang/String;");
+	this->recallMsgM = env->GetStaticMethodID(CPP_lib, "KRecall", "(Ljava/lang/String;)Ljava/lang/String;");
+	this->QueryML = env->GetStaticMethodID(CPP_lib, "KQueryML", "(J)Ljava/lang/String;");
+	this->uploadImg= env->GetStaticMethodID(config->CPP_lib, "KUploadImg", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;");
+	this->muteAll = env->GetStaticMethodID(config->CPP_lib, "KMuteGroup", "(JZ)Ljava/lang/String;");
+	this->getowner = env->GetStaticMethodID(config->CPP_lib, "KQueryOwner", "(J)Ljava/lang/String;");
+	this->buildforward = env->GetStaticMethodID(config->CPP_lib, "KBuildforward", "(Ljava/lang/String;)Ljava/lang/String;");
+	this->NFR = env->GetStaticMethodID(config->CPP_lib, "KNfroperation", "(Ljava/lang/String;Z)Ljava/lang/String;");
+	this->GI = env->GetStaticMethodID(config->CPP_lib, "KGioperation", "(Ljava/lang/String;Z)Ljava/lang/String;");
 }
 Config::~Config() {
 	manager->getEnv()->DeleteGlobalRef(this->CPP_lib);
@@ -95,30 +86,25 @@ throw: InitException 即找不到签名
 */
 void Logger::init(JNIEnv* env){
 	this->CPP_lib = (jclass)(env->NewGlobalRef(env->FindClass("tech/eritquearcus/miraicp/CPP_lib")));
-	this->sinfo = env->GetStaticMethodID(this->CPP_lib, "SendLog", "(Ljava/lang/String;)V");
-	this->swarning = env->GetStaticMethodID(this->CPP_lib, "SendW", "(Ljava/lang/String;)V");
-	this->serror = env->GetStaticMethodID(this->CPP_lib, "SendE", "(Ljava/lang/String;)V");
+	this->log = env->GetStaticMethodID(this->CPP_lib, "KSendLog", "(Ljava/lang/String;I)V");
 	if (this->CPP_lib == NULL) {
 		throw InitException("logger初始化错误", 1);
 	}
-	if (this->sinfo == NULL) {
+	if (this->log== NULL) {
 		throw InitException("logger初始化错误", 2);
 	}
-	if (this->swarning == NULL) {
-		throw InitException("logger初始化错误", 3);
-	}
-	if (this->serror == NULL) {
-		throw InitException("logger初始化错误", 4);
-	}
+}
+void Logger::send0(std::string log, JNIEnv* env, int level) {
+	env->CallStaticVoidMethod(config->CPP_lib, this->log, tools.str2jstring(log.c_str()), (jint)level);
 }
 void Logger::Warning(std::string log, JNIEnv* env) {
-	env->CallStaticVoidMethod(config->CPP_lib, this->swarning, tools.str2jstring(log.c_str()));
+	this->send0(log, env, 1);
 }
 void Logger::Error(std::string log, JNIEnv* env) {
-	env->CallStaticVoidMethod(config->CPP_lib, this->serror, tools.str2jstring(log.c_str()));
+	this->send0(log, env, 2);
 }
 void Logger::Info(std::string log, JNIEnv* env) {
-	env->CallStaticVoidMethod(config->CPP_lib, this->sinfo, tools.str2jstring(log.c_str()));
+	this->send0(log, env, 0);
 }
 Logger::~Logger() {
 	manager->getEnv()->DeleteGlobalRef(this->CPP_lib);
@@ -166,16 +152,16 @@ MessageSource::MessageSource(std::string t) {
 //发送这个聊天记录
 void ForwardMessage::sendTo(Contact* c, JNIEnv* env) {
 	Json::Value temp;
-	temp["id"] = c->id;
-	temp["groupid"] = c->groupid;
-	temp["type"] = c->type;
+	temp["id"] = c->id();
+	temp["groupid"] = c->groupid();
+	temp["type"] = c->type();
 	temp["content"] = sendmsg;
 	std::string re = tools.jstring2str((jstring)env->
 		CallStaticObjectMethod(config->CPP_lib, config->buildforward,
 			tools.str2jstring(tools.JsonToString(temp).c_str())));
 	if (re == "Y") return;
 	if (re == "E1") {
-		switch (c->type) {
+		switch (c->type()) {
 		case 1:
 			throw FriendException();
 		case 2:
@@ -213,13 +199,13 @@ std::string Image::toMiraiCode() {
 
 /*好友类实现*/
 Friend::Friend(unsigned long long id, JNIEnv* env) {
-	this->type = 1;
-	this->id = id;
-	std::string temp = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->NickorNameF, (jlong)id, (jlong)id));
+	this->_type = 1;
+	this->_id = id;
+	std::string temp = LowLevelAPI::getnickornamecard0(this, env);
 	if (temp == "E1") {
 		throw FriendException();
 	}
-	this->nickOrNameCard = temp;
+	this->_nickOrNameCard = temp;
 }
 Image Friend::uploadImg(std::string filename, JNIEnv* env) {
 	std::ifstream fin(filename);
@@ -229,18 +215,18 @@ Image Friend::uploadImg(std::string filename, JNIEnv* env) {
 		throw std::invalid_argument("NO_FILE_ERROR");
 	}
 	fin.close();
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->uploadImgF, (jlong)this->id, tools.str2jstring(filename.c_str())));
+	std::string re = LowLevelAPI::uploadImg0(filename, this, env);
 	return Image(re);
 }
 MessageSource Friend::SendMiraiCode(std::string msg, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2F, tools.str2jstring(msg.c_str()), (jlong)this->id));
+	std::string re = LowLevelAPI::send0(msg, this, true, env);
 	if (re == "E1") {
 		throw FriendException();
 	}
 	return MessageSource(re);
 }
 MessageSource Friend::SendMsg(std::string msg, JNIEnv* env){
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2FM, tools.str2jstring(msg.c_str()), (jlong)this->id));
+	std::string re = LowLevelAPI::send0(msg, this, false, env);
 	if (re == "E1") {
 		throw FriendException();
 	}
@@ -249,14 +235,14 @@ MessageSource Friend::SendMsg(std::string msg, JNIEnv* env){
 
 /*成员类实现*/
 Member::Member(unsigned long long id, unsigned long long groupid, JNIEnv* env) {
-	this->type = 3;
-	this->id = id;
+	this->_type = 3;
+	this->_id = id;
 	this->Mute_id = config->Mute;
-	this->groupid = groupid;
+	this->_groupid = groupid;
 	this->Query_permission = config->QueryP;
 	this->KickM = config->KickM;
-	std::string temp = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->NickorNameM, (jlong)id, (jlong)groupid));
-	this->nickOrNameCard = temp;
+	std::string temp = LowLevelAPI::getnickornamecard0(this, env);
+	this->_nickOrNameCard = temp;
 	this->permission = getPermission();
 	if (temp == "E1") {
 		throw MemberException(1);
@@ -266,7 +252,7 @@ Member::Member(unsigned long long id, unsigned long long groupid, JNIEnv* env) {
 	}
 }
 unsigned int Member::getPermission(JNIEnv* env){
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->QueryP, (jlong)id, (jlong)groupid));
+	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->QueryP, (jlong)id(), (jlong)groupid()));
 	if (re == "E1") {
 		throw MemberException(1);
 	}
@@ -276,7 +262,7 @@ unsigned int Member::getPermission(JNIEnv* env){
 	return stoi(re);
 }
 void Member::Mute(int time, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, this->Mute_id, (jlong)this->id, (jlong)this->groupid, (jint)time));
+	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, this->Mute_id, (jlong)this->id(), (jlong)this->groupid(), (jint)time));
 	if (re == "Y") {
 		return;
 	}
@@ -294,7 +280,7 @@ void Member::Mute(int time, JNIEnv* env) {
 	}
 }
 void Member::Kick(std::string reason, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, this->KickM, (jlong)id, (jlong)groupid, tools.str2jstring(reason.c_str())));
+	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, this->KickM, (jlong)id(), (jlong)groupid(), tools.str2jstring(reason.c_str())));
 	if (re == "Y") {
 		return;
 	}
@@ -315,11 +301,11 @@ Image Member::uploadImg(std::string filename, JNIEnv* env) {
 		throw IOException("文件不存在,位置:C++部分 uploadImg2Group(),文件名:" + filename);
 	}
 	fin.close();
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->uploadImgM, (jlong)groupid, (jlong)id, tools.str2jstring(filename.c_str())));
+	std::string re = LowLevelAPI::uploadImg0(filename, this, env);
 	return Image(re);
 }
 MessageSource Member::SendMiraiCode(std::string msg, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2M, tools.str2jstring(msg.c_str()), (jlong)this->id, (jlong)this->groupid));
+	std::string re = LowLevelAPI::send0(msg, this, true, env);
 	if (re == "E1") {
 		throw MemberException(1);
 	}
@@ -329,7 +315,7 @@ MessageSource Member::SendMiraiCode(std::string msg, JNIEnv* env) {
 	return MessageSource(re);
 }
 MessageSource Member::SendMsg(std::string msg,JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2MM, tools.str2jstring(msg.c_str()), (jlong)this->id, (jlong)this->groupid));
+	std::string re = LowLevelAPI::send0(msg, this, false, env);
 	if (re == "E1") {
 		throw MemberException(1);
 	}
@@ -341,20 +327,18 @@ MessageSource Member::SendMsg(std::string msg,JNIEnv* env) {
 
 /*群聊类实现*/
 Group::Group(unsigned long long id, JNIEnv* env) {
-	this->type = 2;
-	this->id = id;
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib,
-		config->QueryN,
-		(jlong)this->id));
+	this->_type = 2;
+	this->_id = id;
+	std::string re = LowLevelAPI::getnickornamecard0(this, env);
 	if (re == "E1") {
 		throw GroupException();
 	}
-	this->nickOrNameCard = re;
+	this->_nickOrNameCard = re;
 }
 std::vector<unsigned long long> Group::getMemberList() {
 	std::string re = tools.jstring2str((jstring)manager->getEnv()->CallStaticObjectMethod(config->CPP_lib,
 		config->QueryML,
-		(jlong)this->id));
+		(jlong)this->id()));
 	if (re == "E1") {
 		throw GroupException();
 	}
@@ -389,29 +373,29 @@ Image Group::uploadImg(std::string filename, JNIEnv* env) {
 		//throw invalid_argument("NO_FILE_ERROR");
 	}
 	fin.close();
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->uploadImgG, (jlong)this->id, tools.str2jstring(filename.c_str())));
+	std::string re = LowLevelAPI::uploadImg0(filename, this, env);
 	return Image(re);
 }
 void Group::setMuteAll(bool sign, JNIEnv* env){
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->muteAll, (jlong)this->id, (jboolean)sign));
+	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->muteAll, (jlong)this->id(), (jboolean)sign));
 	if (re == "Y") return;
 	if (re == "E1") throw GroupException();
 	if (re == "E2") throw BotException(1);
 }
 Member Group::getOwner(JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->getowner, (jlong)this->id));
+	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->getowner, (jlong)this->id()));
 	if (re == "E1")throw GroupException();
-	return Member(stoi(re), this->id);
+	return Member(stoi(re), this->id());
 }
 MessageSource Group::SendMiraiCode(std::string msg, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2G, tools.str2jstring(msg.c_str()), (jlong)this->id));
+	std::string re = LowLevelAPI::send0(msg, this, true, env);
 	if (re == "E1") {
 		throw GroupException();
 	}
 	return MessageSource(re);
 }
 MessageSource Group::SendMsg(std::string msg, JNIEnv* env) {
-	std::string re = tools.jstring2str((jstring)env->CallStaticObjectMethod(config->CPP_lib, config->SendMsg2GM, tools.str2jstring(msg.c_str()), (jlong)this->id));
+	std::string re = LowLevelAPI::send0(msg, this, false, env);
 	if (re == "E1") {
 		throw GroupException();
 	}
@@ -479,4 +463,17 @@ std::string Tools::JsonToString(const Json::Value& root)
 	std::unique_ptr<Json::StreamWriter> writer(stream_builder.newStreamWriter());
 	writer->write(root, &stream);
 	return stream.str();
+}
+Json::Value Tools::StringToJson(std::string Rcontent) {
+	const auto rawJsonLength = static_cast<int>(Rcontent.length());
+	Json::String err;
+	Json::Value root;
+	Json::CharReaderBuilder builder;
+	const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+	if (!reader->parse(Rcontent.c_str(), Rcontent.c_str() + rawJsonLength, &root,
+		&err)) {
+		//error
+		return NULL;
+	}
+	return root;
 }
--- plugin/src/main/kotlin/CPP_lib.kt
+++ plugin/src/main/kotlin/CPP_lib.kt
@@ -1,21 +1,18 @@
 package tech.eritquearcus.miraicp
+import com.google.gson.Gson
 import kotlinx.coroutines.*
 import tech.eritquearcus.miraicp.PluginMain.BasicSendLog
-import tech.eritquearcus.miraicp.PluginMain.Send
-import tech.eritquearcus.miraicp.PluginMain.dll_name
-import tech.eritquearcus.miraicp.PluginMain.GetN
-import tech.eritquearcus.miraicp.PluginMain.GetNN
+import tech.eritquearcus.miraicp.PluginMain.GetNickOrNameCard
 import tech.eritquearcus.miraicp.PluginMain.QueryImg
 import tech.eritquearcus.miraicp.PluginMain.QueryML
-import tech.eritquearcus.miraicp.PluginMain.QueryNG
 import tech.eritquearcus.miraicp.PluginMain.SendError
-import tech.eritquearcus.miraicp.PluginMain.SendG
-import tech.eritquearcus.miraicp.PluginMain.SendGM
-import tech.eritquearcus.miraicp.PluginMain.SendM
+import tech.eritquearcus.miraicp.PluginMain.SendMiraiCode
+import tech.eritquearcus.miraicp.PluginMain.SendMsg
 import tech.eritquearcus.miraicp.PluginMain.SendWarning
 import tech.eritquearcus.miraicp.PluginMain.accpetFriendRequest
 import tech.eritquearcus.miraicp.PluginMain.accpetGroupInvite
 import tech.eritquearcus.miraicp.PluginMain.buildforwardMsg
+import tech.eritquearcus.miraicp.PluginMain.dll_name
 import tech.eritquearcus.miraicp.PluginMain.getowner
 import tech.eritquearcus.miraicp.PluginMain.kkick
 import tech.eritquearcus.miraicp.PluginMain.kqueryM
@@ -25,9 +22,7 @@ import tech.eritquearcus.miraicp.PluginMain.recallMsg
 import tech.eritquearcus.miraicp.PluginMain.rejectFriendRequest
 import tech.eritquearcus.miraicp.PluginMain.rejectGroupInvite
 import tech.eritquearcus.miraicp.PluginMain.scheduling
-import tech.eritquearcus.miraicp.PluginMain.uploadImgFriend
-import tech.eritquearcus.miraicp.PluginMain.uploadImgGroup
-import tech.eritquearcus.miraicp.PluginMain.uploadImgMember
+import tech.eritquearcus.miraicp.PluginMain.uploadImg
 
 class CPP_lib {
     var ver:String=""
@@ -36,179 +31,110 @@ class CPP_lib {
     }
     //"C:\Program Files\Java\jdk1.8.0_261\bin\javah.exe" org.example.mirai.plugin.CPP_lib
     companion object{
+        private val gson = Gson()
         init {
-            //这里填自己的路径
             System.load(dll_name)
         }
         //send MiraiCode
-        //to Friend
         @JvmStatic
-        fun SendPrivateMSG(message: String, id: Long):String{
+        fun KSend(source: String, miraiCode:Boolean):String{
             return runBlocking {
-                Send(message, id)
-            }
-
-        }
-        //to Member
-        @JvmStatic
-        fun SendPrivateM2M(message: String, id: Long, gid: Long): String{
-            // 反向调用发送消息
-            return runBlocking {
-                Send(message, id, gid)
-            }
-        }
-        //to Group
-        @JvmStatic
-        fun SendGroup(message:String,id:Long):String {
-            return runBlocking {
-                SendG(message, id)
-            }
-        }
-        //send msg(painText)
-        //to Friend
-        @JvmStatic
-        fun SendPrivateMSGM(message: String, id: Long):String{
-            // 反向调用发送消息
-            return runBlocking {
-                SendM(message, id)
-            }
-
-        }
-        //to Member
-        @JvmStatic
-        fun SendPrivateM2MM(message: String, id: Long, gid: Long): String{
-            // 反向调用发送消息
-            return runBlocking {
-                SendM(message, id, gid)
-            }
-        }
-        //to Group
-        @JvmStatic
-        fun SendGroupM(message:String,id:Long):String {
-            return runBlocking {
-                SendGM(message, id)
+                val tmp = gson.fromJson(source, Config.SendRequest::class.java)
+                return@runBlocking when (miraiCode) {
+                    false -> SendMsg(tmp.content, tmp.contact)
+                    true -> SendMiraiCode(tmp.content, tmp.contact)
+                }
             }
         }
         //recall messageSource
         @JvmStatic
-        fun recall(source: String):String {
+        fun KRecall(source: String):String {
             return runBlocking {
                 recallMsg(source)
             }
         }
         //查询图片下载链接
         @JvmStatic
-        fun QueryImgUrl(id:String): String {
+        fun KQueryImgUrl(id:String): String {
             return runBlocking {
                 QueryImg(id)
             }
         }
-        //发送普通日志
         @JvmStatic
-        fun SendLog(log:String) {
-            BasicSendLog(log)
-        }
-        //发送警告日志
-        @JvmStatic
-        fun SendW(log:String) {
-            SendWarning(log)
-        }
-        //发送错误日志
-        @JvmStatic
-        fun SendE(log:String) {
-            SendError(log)
-        }
-        //取昵称
-        @JvmStatic
-        fun GetNick(qqid:Long): String {
-            return GetN(qqid)
-        }
-        //取群名片
-        @JvmStatic
-        fun GetNameCard(qqid:Long, groupid:Long): String {
-            return GetNN(qqid, groupid);
-        }
-        //给群聊上传图片以备发送
-        @JvmStatic
-        fun uploadImgG(qqid: Long, fileName:String): String{
-            return runBlocking {
-                uploadImgGroup(qqid, fileName)
+        fun KSendLog(log:String, level: Int){
+            when(level){
+                0-> BasicSendLog(log)
+                1-> SendWarning(log)
+                2-> SendError(log)
             }
         }
-        //给好友上传图片以备发送
         @JvmStatic
-        fun uploadImgF(qqid: Long, fileName:String): String{
-            return runBlocking {
-                uploadImgFriend(qqid, fileName)
-            }
+        fun KGetNickOrNameCard(source: String): String {
+            return GetNickOrNameCard(gson.fromJson(source, Config.Contact::class.java))
         }
-        //群成员对象上传图片
         @JvmStatic
-        fun uploadImgM(groupid: Long, qqid: Long, fileName:String): String{
+        fun KUploadImg(fileName:String,source:String):String{
             return runBlocking {
-                uploadImgMember(groupid,qqid, fileName)
+                uploadImg(fileName, gson.fromJson(source, Config.Contact::class.java))
             }
         }
         //定时任务
         @JvmStatic
-        fun schedule(time:Long, id:String){
+        fun KSchedule(time:Long, id:String){
             scheduling(time, id)
         }
         //mute member
         @JvmStatic
-        fun muteM(qqid: Long, groupid: Long, time: Int): String{
+        fun KMuteM(qqid: Long, groupid: Long, time: Int): String{
             return runBlocking {
                 mute(qqid, groupid, time)
             }
         }
         //query the permission of a member in a group
         @JvmStatic
-        fun queryM(qqid: Long, groupid: Long): String{
+        fun KQueryM(qqid: Long, groupid: Long): String{
             return kqueryM(qqid, groupid)
         }
         //kick a member
         @JvmStatic
-        fun kickM(qqid: Long, groupid: Long, message: String):String{
+        fun KKickM(qqid: Long, groupid: Long, message: String):String{
             return runBlocking {
                 kkick(qqid, groupid, message)
             }
         }
         //Mute the whole group
         @JvmStatic
-        fun muteGroup(groupid: Long, sign: Boolean):String{
+        fun KMuteGroup(groupid: Long, sign: Boolean):String{
             return muteall(groupid, sign)
-        }
-        //query the group name
-        @JvmStatic
-        fun queryNG(groupid: Long): String {
-            return QueryNG(groupid)
         }
         //query the member list of a group
         @JvmStatic
-        fun queryML(groupid: Long):String{
+        fun KQueryML(groupid: Long):String{
             return QueryML(groupid)
         }
         //query the owner of a group
         @JvmStatic
-        fun queryOwner(groupid: Long):String{
+        fun KQueryOwner(groupid: Long):String{
             return getowner(groupid)
         }
         //build forward message
         @JvmStatic
-        fun buildforward(text:String):String{
+        fun KBuildforward(text:String):String{
             return runBlocking {
                 buildforwardMsg(text)
             }
         }
         @JvmStatic
-        fun nfroperation(text: String, sign: Boolean):String{
+        // new friend request operation
+        fun KNfroperation(text: String, sign: Boolean):String{
             return runBlocking {
                 if (sign) accpetFriendRequest(text)
                 else rejectFriendRequest(text)
             }
         }
+        // Group invite operation
         @JvmStatic
-        fun gioperation(text: String, sign:Boolean):String{
+        fun KGioperation(text: String, sign:Boolean):String{
             return runBlocking {
                 if(sign) accpetGroupInvite(text)
                 else rejectGroupInvite(text)
