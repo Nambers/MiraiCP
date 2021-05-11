4948584ed09b49d7da110e3e2fb8f48983138ec7
Eritque arcus <1930893235@qq.com>
2021-05-11 08:58:32 +0800

    优化文件上传相关接口 Fix #43

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
-    val kotlinVersion = "1.4.30"
-    kotlin("jvm") version kotlinVersion
-    kotlin("plugin.serialization") version kotlinVersion
+    kotlin("jvm") version "1.4.30"
     id("net.mamoe.mirai-console") version "2.6.4"
 }
 
@@ -16,4 +14,5 @@ repositories {
 }
 dependencies{
     implementation("com.google.code.gson:gson:2.8.6")
+    implementation("org.json:json:20210307")
 }
