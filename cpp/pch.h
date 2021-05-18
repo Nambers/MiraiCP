1432259151b8268c87a1dfa0b3ca0e6d10bf4972
Eritque arcus <1930893235@qq.com>
2021-05-18 10:52:35 +0800

    update
    Former-commit-id: b74fecc6bba4108f92a20e0e73f5de440aed3672 [formerly dd3d170b30859b0c1f4f3d1330d0c5602772d086]
    Former-commit-id: 69aca5a7344a7bc25d8081d87d9f586d628c73ef
    Former-commit-id: f733137f6f903bd6dda74bdf2beeb2bd304aa665

--- cpp/pch.h
+++ cpp/pch.h
@@ -1,20 +1,35 @@
 #pragma once
 /*MiraiCP依赖*/
+/// C++ 标准库
 #include <iostream>
+/// Window.h
 #include "framework.h"
+/// string 标准库
 #include <string>
+/// vector STL容器
 #include <vector>
+/// 模板类
 #include <functional>
+/// json解析
 #include "json.hpp"
+/// 转码和判断文件
+#include <filesystem>
+/// JNI接口
 #include "tech_eritquearcus_miraicp_CPP_lib.h"
+/// JNI库
 #include "include/jni.h"
+/// 字符串流
 #include <sstream>
+/// 正则表达式
 #include <regex>
-#include <fstream>
-#include <vector>
+/// 多线程
 #include <thread>
+/// map STL容器
 #include <map>
+/// 线程锁
 #include <mutex>
+/// MiraiCP定义的常量
 #include "constants.h"
+/// MiraiCP接口
 #include "tools.h"
 /*结束*/
