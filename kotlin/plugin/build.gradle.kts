8a993f8a5355712c7ba07b976233f7f76820897e
Eritque arcus <1930893235@qq.com>
2021-06-11 22:54:54 +0800

    ready for v2.6.4
    Former-commit-id: 793b15db91b260f523cd670521d37d3b51ba410d [formerly c56702c18a5bb369c0db3502227e86252c614ec4]
    Former-commit-id: 4614787c29e4cb94a4f0debf8734ef821eb9ec30
    Former-commit-id: b4389a3e45338aa0e7caef6afd648e2d603e510d

--- kotlin/plugin/build.gradle.kts
+++ kotlin/plugin/build.gradle.kts
@@ -1,6 +1,16 @@
 plugins{
     id("net.mamoe.mirai-console")
 }
+
+tasks{
+    afterEvaluate {
+        named<net.mamoe.mirai.console.gradle.BuildMiraiPluginTask>("buildPlugin") {
+            archiveBaseName.set("MiraiCP-plugin")
+            archiveClassifier.set("")
+            archiveVersion.set(V.miraiCP)
+        }
+    }
+}
 dependencies{
     api(project(":shared"))
 }
