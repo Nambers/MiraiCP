d375e59c2fe055132f8d93f815b4f4c9f0cf6239
Eritque arcus <1930893235@qq.com>
2021-06-21 16:26:12 +0800

    update version control
    Former-commit-id: ffea81bc5202ddbfa548d1dbf8e4b31f1967d59a

--- kotlin/buildSrc/src/main/kotlin/V.kt
+++ kotlin/buildSrc/src/main/kotlin/V.kt
@@ -1,5 +1,6 @@
 object V{
     const val kotlinVersion = "1.5.10"
     const val miraiVersion = "2.6.7"
-    const val miraiCP = "2.6.5"
+    const val miraiCPVersion = "2.6.5"
+    const val kotlincoroutinesVersion = "1.5.0"
 }
