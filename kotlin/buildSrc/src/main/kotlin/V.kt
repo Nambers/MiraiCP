87e2c61d250f43314b0dce9b37300a6d888e310b
Eritque arcus <1930893235@qq.com>
2021-06-27 21:54:32 +0800

    shift most dependent version management to TOML(new feature in gradle7) to help dependent bot work, update miraiVersion to 2.7-M1, update gradle7
    Former-commit-id: def9892d9cd3f0be148459c7c37c7ef1057fea8e [formerly 1aac4912701649bae672aa8f62495578ba16c619]
    Former-commit-id: 97324549ebb8440e27fe325e2b5dea36190ddadc
    Former-commit-id: 2ca93042f1eed4cbd87c62bb5c4ea4d9b1bad51c

--- kotlin/buildSrc/src/main/kotlin/V.kt
+++ kotlin/buildSrc/src/main/kotlin/V.kt
@@ -1,6 +1,4 @@
 object V{
     const val kotlinVersion = "1.5.10"
-    const val miraiVersion = "2.6.7"
-    const val miraiCPVersion = "2.6.5"
-    const val kotlincoroutinesVersion = "1.5.0"
+    const val miraiVersion = "2.7-M1"
 }
