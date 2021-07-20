21506a13d20182f48a49a869af6d6ca8761e1353
Eritque arcus <1930893235@qq.com>
2021-07-20 20:57:17 +0800

    update kotlin copyright [1/2]

--- kotlin/buildSrc/src/main/kotlin/V.kt
+++ kotlin/buildSrc/src/main/kotlin/V.kt
@@ -1,4 +1,21 @@
-object V{
+/*
+ * Copyright (C) 2020-2021 Eritque arcus and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version(in your opinion).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+object V {
     const val kotlinVersion = "1.5.10"
     const val miraiVersion = "2.7-M1"
 }
--- kotlin/loader/src/test/kotlin/test.kt
+++ kotlin/loader/src/test/kotlin/test.kt
@@ -1,9 +1,27 @@
+/*
+ * Copyright (C) 2020-2021 Eritque arcus and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version(in your opinion).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package tech.eritquearcus.miraicp.loader
+
 import kotlinx.coroutines.runBlocking
 import net.mamoe.mirai.utils.MiraiInternalApi
 
 @OptIn(MiraiInternalApi::class, net.mamoe.mirai.utils.MiraiExperimentalApi::class)
-fun main(){
+fun main() {
     // qqid, passworld, dllpath, checkupdate
     runBlocking {
         main(arrayOf())
