a3f702892f4627490677516066c6279bd0d49dfb
Eritque arcus <1930893235@qq.com>
2021-08-05 00:23:00 +0800

    加入maven-publish(by Him188)插件，发布到maven以供mcl更方便调用和更新; mirai依赖更新2.7-RC-dev3
    Former-commit-id: 39f177860918865450d2ecc94b7e4322bd13c85f [formerly ee899d9518ce0f6759add422b3ee64904f8e4acf]
    Former-commit-id: c3a1de75fd7f93e2487fbb442393aceedf00d002

--- kotlin/loader/src/test/kotlin/test.kt
+++ kotlin/loader/src/test/kotlin/test.kt
@@ -22,7 +22,6 @@ import net.mamoe.mirai.utils.MiraiInternalApi
 
 @OptIn(MiraiInternalApi::class, net.mamoe.mirai.utils.MiraiExperimentalApi::class)
 fun main() {
-    // qqid, passworld, dllpath, checkupdate
     runBlocking {
         main(arrayOf())
     }
