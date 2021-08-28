ab54bd8049a9077d400b6b77413099232a34a309
Eritque arcus <1930893235@qq.com>
2021-08-28 20:12:12 +0800

    去掉没用的依赖

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -38,8 +38,6 @@ subprojects {
         implementation("org.json:json:20210307")
         compileOnly("net.mamoe:mirai-core-api:2.7.0") // mirai-core 的 API
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-        // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
-        implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     }
 }
 group = "tech.eritquearcus"
