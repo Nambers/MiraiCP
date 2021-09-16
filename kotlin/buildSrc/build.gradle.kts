5cd5bffaf8e8fb45b7a49dbd625d31b0dfd1dbb8
Eritque arcus <1930893235@qq.com>
2021-09-16 21:22:15 +0800

    update build.gradle.kts

--- kotlin/buildSrc/build.gradle.kts
+++ kotlin/buildSrc/build.gradle.kts
@@ -20,5 +20,9 @@ plugins {
 }
 
 repositories {
+    mavenLocal()
     mavenCentral()
+    maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
+    gradlePluginPortal()
+    google()
 }
