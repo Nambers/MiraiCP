78256105d9cd182dd5e55d75bb5ec218fb6102ab
Eritque arcus <1930893235@qq.com>
2021-10-13 22:13:11 +0800

    perf(cpp::MiraiCP.hpp): wrap `shared_ptr`
    using new class `Message` wrap `shared_ptr` in `MessageChain`
    
    
    Former-commit-id: e8abc2cb61786d9ec2a5fd7cc6f187f878aafff6 [formerly 69514861830d6d61d7505221473cd0c03f5129c7]
    Former-commit-id: 0a88ce165c3bfdb2abd92e29e6ead0c5463fb3f4
    Former-commit-id: 9996011073ed6c59e7bd1a3952b27791960af3a3

--- cpp/include/test/test.cpp
+++ cpp/include/test/test.cpp
@@ -82,7 +82,7 @@ public:
 
         // 监听群信息
         Event::processor.registerEvent<GroupMessageEvent>([=](GroupMessageEvent e) {
-            e.group.sendMessage(MessageChain::deserializationFromString(e.message.toMiraiCode()));
+            Logger::logger.info(e.message.toMiraiCode());
             e.group.sendMessage("ss");
             // e.group.sendMsg("x");
             // e.group.sendMsg(tmp.toMiraiCode());
