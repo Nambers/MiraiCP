061b879eb48f330c9a9f007d2b9e986c0caa8ae4
Eritque arcus <1930893235@qq.com>
2021-12-10 22:32:35 +0800

    chore: del unused tests, update changelog
    Former-commit-id: e89affa0529907ae8c0765650f84ba16592d4fc8

--- cpp/demo/test.cpp
+++ cpp/demo/test.cpp
@@ -2,6 +2,29 @@
 #include <MiraiCP.hpp>
 using namespace MiraiCP;
 
+class Main : public CPPPlugin {
+public:
+    // 配置插件信息
+    Main() : CPPPlugin(PluginConfig(
+                     "tech.eritquearcus.miraicp",
+                     "test",
+                     "v1.0",
+                     "Eritque arcus")) {}
+    void onEnable() override {
+        // 监听
+        Event::processor.registerEvent<GroupMessageEvent>([](GroupMessageEvent e) {
+            Logger::logger.info(e.group.uploadImg("C:/Users/19308/desktop/6019893df194b1a92220efa4836d6cc.png").isUploaded(e.bot.id));
+        });
+    }
+
+    void onDisable() override {
+        /*插件结束*/
+    }
+};
+
 // 绑定当前插件实例
 void MiraiCP::enrollPlugin() {
+    MiraiCP::enrollPlugin0(new Main());
 }
+
+
