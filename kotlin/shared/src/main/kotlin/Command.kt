43a725a76f24ad2f6d2301752c4ea32b3cf58182
Eritque arcus <1930893235@qq.com>
2022-01-03 10:48:16 +0800

    chore(kotlin, cpp): update copyright
    happy new year ヽ(´ー｀)ﾉ

--- kotlin/shared/src/main/kotlin/Command.kt
+++ kotlin/shared/src/main/kotlin/Command.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2021. Eritque arcus and contributors.
+ * Copyright (c) 2020 - 2022. Eritque arcus and contributors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
