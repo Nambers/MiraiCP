2590ea81d3a731a6b04fbc8c9a00093ee4879bb5
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-05-30 17:13:06 +0800

    chore(cpp): update method signature #113
    [skip ci]

--- cpp/src/libloader/ktInterface.h
+++ cpp/src/libloader/ktInterface.h
@@ -9,18 +9,16 @@
 
 
 // EXPORT functions
-JNIEXPORT jstring Verify(JNIEnv *env, jobject, jstring id);
+JNIEXPORT jobject Verify(JNIEnv *env, jobject, jstring id, jstring);
 
-JNIEXPORT jstring Event(JNIEnv *env, jobject, jstring content);
+JNIEXPORT jobject Event(JNIEnv *env, jobject, jstring content);
 
 JNIEXPORT jobject PluginDisable(JNIEnv *env, jobject job);
-
-JNIEXPORT jstring returnNull();
 
 int registerMethods(JNIEnv *env, const char *className, const JNINativeMethod *gMethods, int numMethods);
 
 const JNINativeMethod method_table[]{
-        {(char *) "Verify", (char *) "(Ljava/lang/String;)Ljava/lang/String;", (jstring *) Verify},
-        {(char *) "Event", (char *) "(Ljava/lang/String;)Ljava/lang/String;", (jstring *) Event},
+        {(char *) "Verify", (char *) "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Void;", (jstring *) Verify},
+        {(char *) "Event", (char *) "(Ljava/lang/String;)Ljava/lang/Void;", (jstring *) Event},
         {(char *) "PluginDisable", (char *) "()Ljava/lang/Void;", (jobject *) PluginDisable}};
 #endif //MIRAICP_PRO_KTINTERFACE_H
