1d8c7f49861c7d52de73ee10e9b21c4b1a816f5e
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-06-03 18:26:52 +0800

    chore(cpp): update copyright

--- cpp/src/libloader/loaderApi.cpp
+++ cpp/src/libloader/loaderApi.cpp
@@ -1,7 +1,18 @@
+// Copyright (c) 2022 - 2022. Antares, Eritque arcus and contributors.
 //
-// Created by antares on 6/3/22.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or any later version(in your opinion).
 //
-
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #include "loaderApi.h"
 #include "JNIEnvManager.h"
