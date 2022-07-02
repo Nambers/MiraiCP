8f3a8f7244221b16f3c14548c4913a9577be8516
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-07-02 18:31:00 +0800

    chore(cpp): remove todo

--- cpp/src/libloader/JNIEnvManager.h
+++ cpp/src/libloader/JNIEnvManager.h
@@ -14,7 +14,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// todo(Antares): 迁移到libloader
 
 #ifndef MIRAICP_PRO_JNIENVMANAGER_H
 #define MIRAICP_PRO_JNIENVMANAGER_H
