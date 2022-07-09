644e97db0d4c2ec113e81d347722831c4687479d
antares0982@gmail.com <Antares0982@gmail.com>
2022-07-10 00:50:38 +0800

    style: auto build

--- cpp/buildScript/addinclude.py
+++ cpp/buildScript/addinclude.py
@@ -19,7 +19,7 @@
 import io
 import os
 
-ss = """// Copyright (c) 2021-2021. Eritque arcus and contributors.
+ss = """// Copyright (c) 2020 - 2022. Eritque arcus and contributors.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
