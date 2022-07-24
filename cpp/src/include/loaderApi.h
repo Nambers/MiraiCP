1b11cc0bfa63a36e83b8c5ba95b71499f22cae1e
antares <32589030+Antares0982@users.noreply.github.com>
2022-07-24 21:44:33 +0800

    feature(cpp): hide low level loader api

--- cpp/src/include/loaderApi.h
+++ cpp/src/include/loaderApi.h
@@ -18,10 +18,40 @@
 #define MIRAICP_PRO_LOADERAPI_H
 
 
-#include "loaderApiInternal.h"
+#include <string>
+#include <vector>
 
-namespace LibLoader::LoaderApi {
+// for plugin usage
+namespace MiraiCP::LoaderApi {
+    /// @brief 返回所有plugin的id
+    std::vector<std::string> showAllPluginId();
 
-} // namespace LibLoader::LoaderApi
+    /// @brief 启用一个已经加载的插件，仅限有管理权限的插件使用，否则没有任何效果
+    /// @param id 插件id
+    void enablePluginById(const std::string &id);
+
+    /// @brief 禁用一个已经启用的插件，仅限有管理权限的插件使用，否则没有任何效果
+    /// @param id 插件id
+    void disablePluginById(const std::string &);
+
+    /// @brief 启用全部已加载的插件，仅限有管理权限的插件使用，否则没有任何效果
+    void enableAllPlugins();
+
+    /// @brief 禁用全部已启用的插件，仅限有管理权限的插件使用，否则没有任何效果
+    void disableAllPlugins();
+
+    /// @brief 加载新的插件，仅限有管理权限的插件使用，否则没有任何效果
+    /// @param path 插件路径
+    /// @param enableNow 是否立即启用
+    void loadNewPlugin(const std::string &path, bool enableNow);
+
+    /// @brief 卸载一个插件，仅限有管理权限的插件使用，否则没有任何效果
+    /// @param id 插件id
+    void unloadPluginById(const std::string &id);
+
+    /// @brief 重载一个插件，仅限有管理权限的插件使用，否则没有任何效果
+    /// @param id 插件id
+    void reloadPluginById(const std::string &id);
+} // namespace MiraiCP::LoaderApi
 
 #endif //MIRAICP_PRO_LOADERAPI_H
