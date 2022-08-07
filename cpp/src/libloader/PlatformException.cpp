aba7f75189882b9ab6a2d89ed952b3d2d5197ab7
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-08-07 20:01:25 +0800

    chore: finish eventHandler under windows

--- cpp/src/libloader/PlatformException.cpp
+++ cpp/src/libloader/PlatformException.cpp
@@ -1,3 +1,19 @@
+// Copyright (c) 2022. Eritque arcus and contributors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or any later version(in your opinion).
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 //
 // Created by antares on 8/6/22.
 //
@@ -12,9 +28,20 @@
 class EventHandlerPitch {
 public:
     static long __stdcall eventHandler(PEXCEPTION_POINTERS pExceptionPointers) {
-        // todo(ea): add loader task and handle exceptionCode
-        pExceptionPointers->ExceptionRecord->ExceptionCode;
-        LibLoader::OString::errTarget.out();
+        auto pluginName = LibLoader::ThreadController::getPluginIdFromThreadId(std::this_thread::get_id());
+        if (pluginName.empty()) {
+            // test the thread name is jvm
+            char threadName[80];
+            pthread_getname_np(pthread_self(), threadName, 80);
+            if (strcmp(threadName, "libLoader") != 0) {
+                return EXCEPTION_CONTINUE_EXECUTION;
+            }
+            LibLoader::logger.error("libLoader线程遇到致命错误，请向MiraiCP仓库提交您的报错信息以及堆栈信息\n");
+            LibLoader::logger.error("errCod:" + std::to_string(pExceptionPointers->ExceptionRecord->ExceptionCode));
+            exit(1);
+        }
+        LibLoader::logger.error("插件" + pluginName + "遇到致命错误! 线程终止, errCod:" + std::to_string(pExceptionPointers->ExceptionRecord->ExceptionCode));
+        LibLoader::sendPluginException(std::move(pluginName));
         TerminateThread(GetCurrentThread(), 1);
         return EXCEPTION_CONTINUE_EXECUTION;
     }
@@ -58,7 +85,7 @@ class [[maybe_unused]] SignalHandle {
             LibLoader::logger.error("libLoader线程遇到致命错误，请向MiraiCP仓库提交您的报错信息以及堆栈信息");
             exit(1);
         }
-        LibLoader::logger.error("插件" + pluginName + "遇到致命错误！线程终止");
+        LibLoader::logger.error("插件" + pluginName + "遇到致命错误! 线程终止");
         LibLoader::sendPluginException(std::move(pluginName));
 
         pthread_cancel(pthread_self());
