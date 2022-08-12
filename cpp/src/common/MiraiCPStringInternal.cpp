779a59019e9ed76e595eaf5e0b7dcc5e1b26d8c9
antares0982@gmail.com <Antares0982@gmail.com>
2022-08-12 20:18:16 +0800

    perf(libloader): assert
    Former-commit-id: d030d7885b2617dc6921a956bda1c060616ac953

--- cpp/src/common/MiraiCPStringInternal.cpp
+++ cpp/src/common/MiraiCPStringInternal.cpp
@@ -14,6 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
+#include "commonTools.h"
 #include "MiraiCPStringInternal.h"
 #ifdef MIRAICP_LIB_LOADER
 #include "LoaderLogger.h"
