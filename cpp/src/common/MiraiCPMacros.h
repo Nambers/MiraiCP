81e065effe2d097770b52270f09990130d957af4
antares <32589030+Antares0982@users.noreply.github.com>
2022-09-23 21:54:13 +0800

    fix(cpp): typo
    Former-commit-id: 5612fc43c860cc40e1c371bab03a68c19f665e5d

--- cpp/src/common/MiraiCPMacros.h
+++ cpp/src/common/MiraiCPMacros.h
@@ -162,10 +162,10 @@ static_assert(false, "Unsupported platform");
 
 #define DECL_GETTER(attr) const decltype(DataType::_##attr) &attr();
 #define IMPL_GETTER(attr)                                                                 \
-    const decltype(LOC_CLASS_NAMESPACE::DataType::_##attr& LOC_CLASS_NAMESPACE::attr(){ \
-        InternalData->request_refresh(); \
-        std::shared_lock<std::shared_mutex> local_lck(InternalData->get_mutex()); \
-        return GetDataInternal()->_##attr;\
+    const decltype(LOC_CLASS_NAMESPACE::DataType::_##attr) &LOC_CLASS_NAMESPACE::attr() { \
+        InternalData->request_refresh();                                                  \
+        std::shared_lock<std::shared_mutex> local_lck(InternalData->get_mutex());         \
+        return GetDataInternal()->_##attr;                                                \
     }
 #define INLINE_GETTER(attr) \
     const auto &attr() { return GetDataInternal()->_##attr; }
