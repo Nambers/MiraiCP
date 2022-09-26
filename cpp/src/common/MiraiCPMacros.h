b3115de341f6b9fcba411b9310b53a2c3bb747b2
antares <32589030+Antares0982@users.noreply.github.com>
2022-09-27 01:24:33 +0800

    fix(cpp): copy the value in getter to avoid data inconsistency

--- cpp/src/common/MiraiCPMacros.h
+++ cpp/src/common/MiraiCPMacros.h
@@ -160,14 +160,14 @@ static_assert(false, "Unsupported platform");
 // getter
 // need to define macro LOC_CLASS_NAMESPACE to the class first!
 
-#define DECL_GETTER(attr) const decltype(DataType::_##attr) &attr();
+#define DECL_GETTER(attr) decltype(DataType::_##attr) attr();
 #define IMPL_GETTER(attr)                                                                 \
-    const decltype(LOC_CLASS_NAMESPACE::DataType::_##attr) &LOC_CLASS_NAMESPACE::attr() { \
+    decltype(LOC_CLASS_NAMESPACE::DataType::_##attr) LOC_CLASS_NAMESPACE::attr() { \
         InternalData->request_refresh();                                                  \
         std::shared_lock<std::shared_mutex> local_lck(InternalData->get_mutex());         \
         return GetDataInternal()->_##attr;                                                \
     }
 #define INLINE_GETTER(attr) \
-    const auto &attr() { return GetDataInternal()->_##attr; }
+    auto attr() { return GetDataInternal()->_##attr; }
 
 #endif //MIRAICP_PRO_MIRAICPMACROS_H
