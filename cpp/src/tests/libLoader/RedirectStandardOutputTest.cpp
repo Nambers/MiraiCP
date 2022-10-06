4952454d13f333d54eee32cbf711df32be8ae3ea
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-10-06 11:03:33 -0400

    fix: fix wrong include header [skip ci]
    Former-commit-id: 1dbe3d512e590cd0a1e81c681104b969cd32cc25

--- cpp/src/tests/libLoader/RedirectStandardOutputTest.cpp
+++ cpp/src/tests/libLoader/RedirectStandardOutputTest.cpp
@@ -14,7 +14,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "common/LoaderLogger.h"
+#include "LoaderLogger.h"
 #include "MiraiCPMacros.h"
 #include "EnvMocker.h"
 #include "redirectCout.h"
