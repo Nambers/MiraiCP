673f3ca8c034e472f35a14d680c50508d38a5ac0
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2022-10-21 12:01:15 -0400

    chore: update mirai version

--- kotlin/buildSrc/src/main/kotlin/Version.kt
+++ kotlin/buildSrc/src/main/kotlin/Version.kt
@@ -23,7 +23,7 @@ object Version {
     const val shadowJar = "7.1.2"
     const val `kotlinx-coroutines-core` = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
 
-    const val mirai = "2.13.0-RC2-dev-7eac75bb"
+    const val mirai = "2.13.0-RC2"
     const val `mirai-core-api` = "net.mamoe:mirai-core-api:${mirai}"
     const val `mirai-core-utils` = "net.mamoe:mirai-core-utils:${mirai}"
     const val `mirai-console` = "net.mamoe:mirai-console:${mirai}"
