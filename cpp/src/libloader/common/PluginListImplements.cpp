d58bf1710194e451a087c5665d08ad1fc4e76b3f
antares <32589030+Antares0982@users.noreply.github.com>
2022-10-27 03:12:28 +0800

    fix(cpp): #137

--- cpp/src/libloader/common/PluginListImplements.cpp
+++ cpp/src/libloader/common/PluginListImplements.cpp
@@ -30,29 +30,34 @@
 #if MIRAICP_WINDOWS
 #include <filesystem>
 #include <windows.h>
+#include "utf8.h"
 // https://stackoverflow.com/questions/1387064/how-to-get-the-error-message-from-the-error-code-returned-by-getlasterror
 //Returns the last Win32 error, in string format. Returns an empty string if there is no error.
 std::string GetLastErrorAsString() {
     //Get the error message ID, if any.
     DWORD errorMessageID = ::GetLastError();
     if (errorMessageID == 0) {
-        return std::string(); //No error message has been recorded
+        return {}; //No error message has been recorded
     }
 
-    LPSTR messageBuffer = nullptr;
+    LPWSTR messageBuffer = nullptr;
 
     //Ask Win32 to give us the string version of that message ID.
     //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-                                 NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR) &messageBuffer, 0, NULL);
+    size_t size = FormatMessageW(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                                 nullptr, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPWSTR) &messageBuffer, 0, nullptr);
 
     //Copy the error message into a std::string.
-    std::string message(messageBuffer, size);
+    std::wstring message(messageBuffer, size);
 
     //Free the Win32's string's buffer.
     LocalFree(messageBuffer);
 
-    return message;
+    // Translate to utf8 encoded std::string
+    std::string ans;
+    utf8::utf16to8(message.begin(), message.end(), std::back_inserter(ans));
+
+    return ans;
 }
 #endif
 
