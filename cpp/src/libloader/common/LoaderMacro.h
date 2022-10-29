e52163f618c42b8d01460c239f40a33e460fa2e4
antares <32589030+Antares0982@users.noreply.github.com>
2022-10-30 00:19:41 +0800

    refactor(libloader): revert native code, 3 try

--- cpp/src/libloader/common/LoaderMacro.h
+++ cpp/src/libloader/common/LoaderMacro.h
@@ -11,12 +11,19 @@
 #define JSTRING const char *
 #define J_TO_STD_STRING std::string
 #define SET_ENV(x)
+#define LOADER_EXPORT
+#define DECL_EXPORT_NAME(x) x##Impl
+#define J_ARGS
 #else
+#include <jni.h>
 #define JRETURNTYPE jobject
 #define JRETURNVAL nullptr
 #define JSTRING jstring
 #define J_TO_STD_STRING LibLoader::jstring2str
 #define SET_ENV(x) JNIEnvManager::setEnv(x)
+#define LOADER_EXPORT JNIEXPORT
+#define DECL_EXPORT_NAME(x) x
+#define J_ARGS JNIEnv *, jobject
 #endif
 
 #endif //MIRAICP_PRO_LOADERMACRO_H
