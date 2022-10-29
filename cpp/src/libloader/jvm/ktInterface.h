43f37d8f5a31b9654052f3d9d82be87a1f0ed590
antares <32589030+Antares0982@users.noreply.github.com>
2022-10-30 00:19:41 +0800

    refactor(libloader): revert native code, 3 try
    Former-commit-id: e52163f618c42b8d01460c239f40a33e460fa2e4

--- cpp/src/libloader/jvm/ktInterface.h
+++ cpp/src/libloader/jvm/ktInterface.h
@@ -18,20 +18,30 @@
 #define MIRAICP_PRO_KTINTERFACE_H
 
 
-#include <jni.h>
+#include "LoaderMacro.h"
 
 
 // EXPORT functions
-JNIEXPORT jobject Verify(JNIEnv *, jobject, jstring, jstring);
+LOADER_EXPORT JRETURNTYPE DECL_EXPORT_NAME(Verify)(J_ARGS, JSTRING, JSTRING);
 
-JNIEXPORT jobject Event(JNIEnv *, jobject, jstring);
+LOADER_EXPORT JRETURNTYPE DECL_EXPORT_NAME(Event)(J_ARGS, JSTRING);
 
-JNIEXPORT jobject PluginDisable(JNIEnv *, jobject);
+LOADER_EXPORT JRETURNTYPE DECL_EXPORT_NAME(PluginDisable)(J_ARGS);
 
+#ifdef LOADER_NATIVE
+namespace LoaderAPIs {
+    using LogFunc = void (*)(const char *, int);
+    using OperFunc = const char *(*)(const char *);
+    extern LogFunc log;
+    extern OperFunc oper;
+};
+#else
 int registerMethods(JNIEnv *env, const char *className, const JNINativeMethod *gMethods, int numMethods);
 
 const JNINativeMethod method_table[]{
         {(char *) "Verify", (char *) "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/Void;", (jstring *) Verify},
         {(char *) "Event", (char *) "(Ljava/lang/String;)Ljava/lang/Void;", (jstring *) Event},
         {(char *) "PluginDisable", (char *) "()Ljava/lang/Void;", (jobject *) PluginDisable}};
+#endif
+
 #endif //MIRAICP_PRO_KTINTERFACE_H
