4ecb738dd85fbbb2d0799afe05ba469d0faa9ba9
antares0982@gmail.com <Antares0982@gmail.com>
2022-11-15 14:27:09 +0800

    feature(cpp): use make_unique to enroll plugin; add std::chrono schedule overloading

--- cpp/src/include/Schedule.h
+++ cpp/src/include/Schedule.h
@@ -5,17 +5,26 @@
 #ifndef MIRAICP_PRO_SCHEDULE_H
 #define MIRAICP_PRO_SCHEDULE_H
 
+#include <chrono>
 #include <string>
 
 
 namespace MiraiCP {
     /*!
      * @brief 定时任务, 在一定时间后广播**一次**TimeOutEvent
-     * @param time 在多少毫秒后执行
+     * @param sec 在多少秒后执行
      * @param msg 附加的string类型信息
-     * @doxygenEg{1017, schedule.cpp, 定时任务}
+     * @doxygenEg{1017, Schedule.h, 定时任务}
      */
-    void schedule(size_t time, std::string msg);
+    void schedule(size_t sec, std::string msg);
+
+    /*!
+     * @brief 定时任务, 在一定时间后广播**一次**TimeOutEvent，chrono重载
+     * @param sec 在多少秒后执行
+     * @param msg 附加的string类型信息
+     * @doxygenEg{1017, Schedule.h, 定时任务}
+     */
+    void schedule(std::chrono::seconds sec, std::string msg);
 } // namespace MiraiCP
 
 
