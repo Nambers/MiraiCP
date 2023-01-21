ce2f0821f0b2cc37286ac560e66492ad0e14ff4a
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2023-01-21 14:33:15 -0500

    chore: update comments and apiDoc

--- cpp/include/libloader/BS_thread_pool.hpp
+++ cpp/include/libloader/BS_thread_pool.hpp
@@ -1,3 +1,24 @@
+// Copyright (c) 2023. Eritque arcus and contributors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or any later version(in your opinion).
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+/**
+ *  Modified version of Barak Shoshany's thread pool library
+ *  link: https://github.com/bshoshany/thread-pool
+ *  License: MIT
+ */
 #pragma once
 
 /**
@@ -556,8 +577,7 @@ namespace BS {
             threadIndexCounter = index;
             LibLoader::logger.warning("resetting thread in pool at index " + std::to_string(index)+" due to a user caused error");
             new (&threads[index]) std::thread(&thread_pool::worker, this);
-            while (threadIndexCounter != index + 1)
-                ;
+            while (threadIndexCounter != index + 1);
             threadIndexCounter = oldThCounter;
         }
 
