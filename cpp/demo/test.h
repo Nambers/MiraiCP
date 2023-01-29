2ee8dd1361f9bc7cacba40e452caae5199028d30
antares0982@gmail.com <Antares0982@gmail.com>
2023-01-30 00:25:46 +0800

    refactor(cpp): cmake refactor 2
    Former-commit-id: a71a535641041e83234e96f09d4b24613399d4e2

--- cpp/demo/test.h
+++ cpp/demo/test.h
@@ -1,5 +1,4 @@
-
-// Copyright (c) 2022. Eritque arcus and contributors.
+// Copyright (C) 2020-2023 Eritque arcus and contributors.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
@@ -13,14 +12,6 @@
 //
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-
-#include "CPPPlugin.h"
-#include "Event.h"
-#include "Member.h"
-#include "Schedule.h"
-#include "ThreadTask.h"
-#include "utils.h"
 
 using namespace MiraiCP;
 
@@ -32,25 +23,6 @@ const PluginConfig CPPPlugin::config{
         "测试中文描述", // 可选：插件描述
                         // 可选：日期
 };
-
-// 测试全局变量构造
-Member mem(nlohmann::json{{"id", 1234567},
-                          {"botId", 1245634},
-                          {"groupId", 123456}});
-Member mem2(123, 456, 789);
-
-// test
-
-size_t test_task(std::chrono::seconds sleeptime) {
-    std::this_thread::sleep_for(sleeptime);
-    Logger::logger.info("Wake!");
-    return sleeptime.count();
-}
-
-void test_task2() {
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    Logger::logger.info("Wake2!");
-}
 
 class Main : public CPPPlugin {
 public:
@@ -69,20 +41,6 @@ public:
             auto r = a.group.getFileById("/e3080b6d-aea9-4d66-9f07-7289cf097cdc");
             Logger::logger.info("file size: ", r.size);
         });
-        Event::registerEvent<TimeOutEvent>([](TimeOutEvent a) {
-            Logger::logger.info("timeout");
-            Logger::logger.info(a.msg);
-        });
-        schedule(1, "This is a message");
-        auto fu = ThreadTask::promiseTask(test_task, std::chrono::seconds(2));
-        ThreadTask::addTask(test_task2);
-        ThreadTask::addTask(test_task, std::chrono::seconds(1));
-        size_t t = fu.get();
-        Logger::logger.info("Get future: " + std::to_string(t));
-        ////
-        // LoaderApi::disableAllPlugins();
-        //        int *p = nullptr;
-        //        *p = 1;
     }
 
     void onDisable() override {
---
+++ cpp/demo/test_multi.cpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2020-2023 Eritque arcus and contributors.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or any later version(in your opinion).
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/// dev: add includes here before including test.h, to share code with target "multi"
+
+#include "CPPPlugin.h"
+#include "Event.h"
+#include "Member.h"
+#include "Schedule.h"
+#include "ThreadTask.h"
+#include "utils.h"
+// --------------------
+#include "test.h"
