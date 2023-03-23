b5c8f6282d1df79b77dbeb666b8a375732deec25
Eritque arcus <eritque-arcus@eritque-arcus.tech>
2023-03-23 18:14:19 -0400

    chore: update kotlin and mirai version
    Former-commit-id: 89da11197dee162380c9b6b3ba09fb945f469c48

--- kotlin/buildSrc/src/main/kotlin/Version.kt
+++ kotlin/buildSrc/src/main/kotlin/Version.kt
@@ -19,12 +19,12 @@
 object Version {
     const val miraiCP = "2.14.0"
     // plugins
-    const val kotlin = "1.8.0"
+    const val kotlin = "1.8.10"
     const val shadowJar = "7.1.2"
     const val `kotlinx-coroutines-core` = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
     const val `kotlin-test` = "org.jetbrains.kotlin:kotlin-test:$kotlin"
 
-    const val mirai = "2.14.0"
+    const val mirai = "2.15.0-M1"
     const val `mirai-core-api` = "net.mamoe:mirai-core-api:$mirai"
     const val `mirai-core-utils` = "net.mamoe:mirai-core-utils:$mirai"
     const val `mirai-console` = "net.mamoe:mirai-console:$mirai"
