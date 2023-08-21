7ed135c4ed802864fc9dcbc4d28276fbba2b7e27
eritque0arcus <eritque-arcus@eritque-arcus.tech>
2023-08-21 18:14:07 -0400

    feat: add noqa to skip specific platform, close #199
    Former-commit-id: bf8d7242a822cb8bbf9f4298b2780b0f90bd5dc7

---
+++ scripts/CIHelper.py
@@ -0,0 +1,44 @@
+# -*- utf-8 -*-
+# author: eritque-arcus
+# date: 08/21/23
+
+import sys, json, regex
+
+def genMatrix(msg: str):
+    global winOS, linuxOS
+    matrix = {
+        "os":[winOS, linuxOS], 
+        "include":[
+            {"os": winOS, "targetName": "MSVCX64"},
+            {"os": linuxOS, "targetName": "LinuxX64"}
+            ]
+        }
+    if msg.endswith("noqa"):
+        matrix = {"os": []}
+    else:
+        re = regex.findall(R"noqa\(([a-z,]+)\)$", msg)[0]
+        if re is not None:
+            OSs = re.split(',')
+            for OS in OSs:
+                if OS == "win" or OS == "windows":
+                    matrix["os"].remove(winOS)
+                    matrix["include"] = [item for item in matrix["include"] if item["os"] != winOS]
+                elif OS == "linux":
+                    matrix["os"].remove(linuxOS)
+                    matrix["include"] = [item for item in matrix["include"] if item["os"] != linuxOS]
+                else:
+                    print("Error: Can't find specific noqa OS name:" + OS +"!!!!", file=sys.stderr)
+    print("matrix=" + json.dumps(matrix))
+
+if __name__ == "__main__":
+    winOS = "windows-2019"
+    linuxOS = "ubuntu-20.04"
+    args = sys.argv
+    assert(len(args) >= 2)
+    cmd = args[1]
+    if cmd == "genMatrix":
+        assert(len(args) == 3)
+        genMatrix(args[2])
+    else:
+        print("cmd not match!", file=sys.stderr)
+        exit(1)
